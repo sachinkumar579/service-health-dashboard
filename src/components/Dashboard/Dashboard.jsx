c46953e929edb0ce7826e4a09533997d3903c34f
sachin kumar <ssachin579@gmail.com>
2021-12-04 19:48:40 +0530

    the code

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  let services = [
+    {
+      name: "App 123 Test",
+      status: "UP",
+      url: "http://localhost:8080/app",
+    },
+    {
+      name: "App 456 Test",
+      status: "UP",
+      url: "http://localhost:8080/app",
+    },
+    {
+      name: "App 789 Test",
+      status: "UP",
+      url: "http://localhost:8080/app",
+    },
+    {
+      name: "App 011 Test",
+      status: "DOWN",
+      url: "http://localhost:8080/app",
+    },
+    {
+      name: "App 121 Test",
+      status: "DOWN",
+      url: "http://localhost:8080/app",
+    }, {
+      name: "App 123 Test",
+      status: "UP",
+      url: "http://localhost:8080/app",
+    },
+    {
+      name: "App 456 Test",
+      status: "UP",
+      url: "http://localhost:8080/app",
+    },
+    {
+      name: "App 789 Test",
+      status: "UP",
+      url: "http://localhost:8080/app",
+    },
+    {
+      name: "App 011 Test",
+      status: "DOWN",
+      url: "http://localhost:8080/app",
+    },
+    {
+      name: "App 121 Test",
+      status: "DOWN",
+      url: "http://localhost:8080/app",
+    },
+  ];
+
+  const [serviceNames, setServiceName] = useState(services);
+
+  const onServiceNameChangeHandler = (event) => {
+    let servicesFiltered = services.filter((data) => {
+      if (data.name.toLowerCase().includes(event.target.value.toLowerCase()))
+        return true;
+    });
+
+    if (event.target.value.trim() === 0) {
+      setServiceName(service);
+    } else {
+      setServiceName(servicesFiltered);
+    }
+  };
+
+  return (
+    <div className="table">
+      <input
+        type="text"
+        placeholder="search service"
+        className="input"
+        onChange={onServiceNameChangeHandler}
+      ></input>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>Service</th>
+            <th>Status</th>
+            <th>URL</th>
+          </tr>
+          {serviceNames.length === 0 ? (
+            <tr>
+              <td></td>
+              <td>No matching services</td>
+              <td></td>
+              <td></td>
+            </tr>
+          ) : (
+            serviceNames.map((data, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{data.name}</td>
+                <td>
+                  {data.status === "UP" ? (
+                    <span className="greenStatus"></span>
+                  ) : (
+                    <span className="redStatus"></span>
+                  )}
+                </td>
+                <td>{data.url}</td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import React from "react";
+import "./Login.css";
+import md5 from "md5";
+import ErrorModal from "../Modals/ErrorModal";
+import Card from '../UI/Card.jsx'
+
+const Login = ({ setToken }) => {
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState();
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if ((username.length !== 0 && password.length !== 0) &&
+      md5(username) === "21232f297a57a5a743894a0e4a801fc3" &&
+      md5(password) === "21232f297a57a5a743894a0e4a801fc3"
+    ) {
+      setToken({ token: 12345 });
+    } else {
+      setUserName("");
+      setPassword("");
+      setError({
+        title: "Invalid username",
+        message: "Please enter valid credentials",
+      });
+    }
+  };
+
+  return (
+    <Card>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        ></ErrorModal>
+      )}
+      <div className="loginForm">
+        <form onSubmit={handleSubmit}>
+          <p> User name </p>
+          <input
+            type="text"
+            onChange={(e) => setUserName(e.target.value)}
+            value={username}
+          ></input>
+          <p> Password </p>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          ></input>
+          <button type="submit" className="loginButton">
+            Log In
+          </button>
+        </form>
+      </div>
+    </Card >
+  );
+};
+
+export default Login;
